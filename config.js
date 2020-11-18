e65cc1cb9f914ecf791b2da440484450eb2a4005
OT <o.trotman5@gmail.com>
2020-11-18 18:00:24 -0400

    5th commit

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ export default {
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/node-rest-shop-db', 
     //sets the value of Config Vars(heroku) which is accessed by process.env to MONGODB_URL
 
-    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
+    //JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 
 }
