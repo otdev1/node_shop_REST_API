b3d15c00e1bc9e0c6ba1564d3c96c7c477888798
OT <o.trotman5@gmail.com>
2020-11-18 18:41:31 -0400

    8th commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan'); //morgan logs activities calling the next() function
 const bodyParser = require('body-parser');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 //const config = require('./config');
 
