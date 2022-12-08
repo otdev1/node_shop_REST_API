a75e5a6caf2e55dd94e7f5bb04c9d88d75695355
otdev1 <66022538+otdev1@users.noreply.github.com>
2022-12-08 18:47:56 -0400

    Update user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+require('dotenv').config()
 
 const User = require("../models/userModel");
 
